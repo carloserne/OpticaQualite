415675c66f71defc854669ccc70f0966496bf286
carloserne <84287252+carloserne@users.noreply.github.com>
2022-07-25 18:18:04 -0500

    Comprobar que no contengan numeros los input

--- web/Modulos/moduloEmp/controller_Empleados.js
+++ web/Modulos/moduloEmp/controller_Empleados.js
@@ -10,13 +10,13 @@ fetch("Modulos/moduloEmp/data_Empleados.json")
         })
         .then(function (jsondata) {
             emps = jsondata;
-            cargarEmptbl();
         }
         );
 
 export function cargarEmp() {
-
+    let pos = 0;
     emp.forEach(function (empleado) {
+        console.log(pos);
         registroEmp =
                 '<tr>' +
                 '<td>' + empleado.ClaveUnicaEmp + '</td>' +
@@ -34,6 +34,8 @@ export function cargarEmp() {
         cuerpotblEmp += registroEmp;
         countIdPersona = parseInt(empleado.Persona.IdPersona);
         countIdEmpleado = parseInt(empleado.IdEmpleado);
+        pos++;
+
     });
     document.getElementById("tblEmpleados").innerHTML = cuerpotblEmp;
     cuerpotblEmp = "";
@@ -79,14 +81,23 @@ export function registrar() {
 }
 
 export function eliminar() {
+
     fetch("./Modulos/moduloEmp/view_eliminarEmpleado.html")
             .then(function (respuesta) {
                 return respuesta.text();
             })
             .then(function (html) {
                 document.getElementById("contenedorGestion").innerHTML = html;
-
+                import ("./controller_Empleados.js").then(
+                        function (controller) {
+                            moduloEmp = controller;
+                            moduloEmp.cargarEmp();
+                            $('#table_id').DataTable();
+                            document.getElementById("table_id_filter").style.display = "block";
+                        }
+                );
             });
+
 }
 
 export function catalogo() {
@@ -100,21 +111,61 @@ export function catalogo() {
             });
 }
 export function registrarNuevo() {
-    Swal.fire({
-        title: '¿Seguro de registrar al empleado?',
-        showDenyButton: true,
-        confirmButtonText: 'Si',
-        denyButtonText: `No`,
-    }).then((result) => {
-        /* Read more about isConfirmed, isDenied below */
-        if (result.isConfirmed) {
-            Swal.fire('Epleado registrado con exito!', '', 'success')
-            alacenarDatos();
-        } else if (result.isDenied) {
+    let inputs = document.getElementsByTagName("input");
+    let input;
+    let vacios = 0;
+    let comprobarString = true;
+    for (var i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+        
+        if (input.value === "") {
             
+            vacios++;
         }
-    })
+        if(input.name !== "telCasa" && input.name !== "telMovil" && input.name !== "contrasena"){
+                if(isNaN(input.value) === false ){
+                    comprobarString = false;
+                }
+                
+            }  
+    }
+    
+    if (vacios === 0) {
+        if(comprobarString === true){
+            Swal.fire({
+            title: '¿Seguro de registrar al empleado?',
+            showDenyButton: true,
+            confirmButtonText: 'Si',
+            denyButtonText: `No`
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('Epleado registrado con exito!', '', 'success')
+                alacenarDatos();
+            } else if (result.isDenied) {
+
+            }
+        });
+        }else{
+            Swal.fire({
+            title: 'Algo salio mal',
+            text: "Asegurese de que ningun campo que no requiera numeros no contenga ningun número!",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar!'
+        })
+        }
+        
+    } else {
+        Swal.fire({
+            title: 'Algo salio mal',
+            text: "Asegurese de llenar todos los campos!",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar!'
+        })
+    }
 }
+
 export function alacenarDatos() {
     let numeroUnicoEmpleado = document.getElementById("numeroUnicoEmpleado").value;
     let nombres = document.getElementById("nombres").value;
