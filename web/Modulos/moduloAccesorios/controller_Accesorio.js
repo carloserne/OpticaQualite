69d48ef1eaba2100e3a1959047d669ceaf4d7a1d
SamQuintana <21000155@alumnos.utleon.edu.mx>
2022-07-25 23:27:41 -0500

    Cambios 8

--- web/Modulos/moduloAccesorios/controller_Accesorio.js
+++ web/Modulos/moduloAccesorios/controller_Accesorio.js
@@ -8,9 +8,11 @@ fetch("Modulos/moduloAccesorios/data_Accesorios.json")
             return response.json();
         })
         .then(function (jsondata) {
+
             accesorios = jsondata;
-            console.log(accesorios);
             cargarAccesoriostbl();
+            accesorios = jsondata;
+
         }
         );
        
@@ -82,4 +84,89 @@ export function catalogo(){
         document.getElementById("contenedorGestion").innerHTML = html;
 
         });
+}
+
+export function registrarNuevo() {
+    let inputs = document.getElementsByTagName("input");
+    let input;
+    let vacios = 0;
+    let comprobarString = true;
+    for (var i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+        
+        if (input.value === "") {
+            
+            vacios++;
+        }
+ 
+    }
+    
+    if (vacios === 0) {
+        if(comprobarString === true){
+            Swal.fire({
+            title: '¿Seguro de registrar el accesorio?',
+            showDenyButton: true,
+            confirmButtonText: 'Si',
+            denyButtonText: `No`
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire('!Accesorio registrado con exito!', '', 'success')
+                alacenarDatos();
+            } else if (result.isDenied) {
+
+            }
+        });
+        }else{
+            Swal.fire({
+            title: 'Algo salio mal',
+            text: "Asegurese de que ningun campo que no requiera números no contenga ningun número!",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar!'
+        })
+        }
+        
+    } else {
+        Swal.fire({
+            title: 'Algo salio mal',
+            text: "Asegurese de llenar todos los campos!",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar!'
+        })
+    }
+}
+
+export function almacenarDatos() {
+    let nombreAccesorio = document.getElementById("nombreAccesorio").value;
+    let marcaAccesorio = document.getElementById("marcaAccesorio").value;
+    let precioCompra = document.getElementById("precioCompra").value;
+    let precioVenta = document.getElementById("precioVenta").value;
+    let existencia = document.getElementById("existencia").value;
+
+    let persona = {
+        "IdPersona": idPersona,
+        "Nombre": nombres,
+        "ApellidoPaterno": apellidoP,
+        "ApellidoMaterno": apellidoM,
+        "Genero": genero,
+        "RFC": rfc,
+        "TelCasa": telCasa,
+        "TelMovil": telMovil,
+        "Correo": correo,
+        "Estatus": 1
+    };
+    let accesorio = {
+        "nombreAccesorio": nombreAccesorio,
+        "marcaAcceorio": marcaAccesorio,
+        "precioCompra": precioCompra,
+        "precioVenta": precioVenta,
+        "existencias": existencia,
+        "estatus": 1
+    };
+
+    accesorio.push(accesorio);
+    accesorios.push(accesorio);
+
+    accesorio();
 }
