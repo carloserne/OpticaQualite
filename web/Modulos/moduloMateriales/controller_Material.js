d3d670110da5c2414975c1cb6904ea8ad159b32f
SamQuintana <21000155@alumnos.utleon.edu.mx>
2022-07-26 22:22:32 -0500

    cambios 12

--- web/Modulos/moduloMateriales/controller_Material.js
+++ web/Modulos/moduloMateriales/controller_Material.js
@@ -1,10 +1,10 @@
 let cuerpotblMateriales = "";
 let registroMateriales;
 let materiales = [];
-let countNumeroUnicoMaterial;
+let posMaterial;
 
 
-fetch("Modulos/moduloAccesorios/data_Accesorios.json")
+fetch("Modulos/moduloMateriales/data_Material.json")
         .then(response => {
             return response.json();
         })
@@ -12,23 +12,21 @@ fetch("Modulos/moduloAccesorios/data_Accesorios.json")
 
             materiales = jsondata;
             cargarMaterialestbl();
-            materiales = jsondata;
 
         }
         );
        
 export function cargarMaterial() {
      
-    material.forEach(function (material) {
+    materia.forEach(function (materialess) {
         registroMateriales =
                 '<tr>' +
-                '<td>' + material.numeroUnicoMaterial + '</td>' +
-                '<td>' + material.nombreMaterial + '</td>' +
-                '<td>' + material.precioCompra + '</td>' +
-                '<td>' + material.precioVenta + '</td>' +
-                '<td>' + material.estatus + '</td>';
+                '<td>' + materialess.numeroUnicoMaterial + '</td>' +
+                '<td>' + materialess.nombreMaterial + '</td>' +
+                '<td>' + materialess.precioCompra + '</td>' +
+                '<td>' + materialess.precioVenta + '</td>' +
+                '<td>' + materialess.estatus + '</td>';
         cuerpotblMateriales += registroMateriales;
-        countNumeroUnicoMaterial = parseInt
     });
     document.getElementById("tblMateriales").innerHTML = cuerpotblMateriales;
     cuerpotblMateriales = "";
@@ -38,19 +36,40 @@ export function cargarMaterial() {
 
 export function cargarMaterialestbl() {
      
-    materiales.forEach(function (material) {
+    materiales.forEach(function (materialess) {
         registroMateriales =
                 '<tr>' +
-                '<td>' + material.numeroUnicoMaterial + '</td>' +
-                '<td>' + material.nombreMaterial + '</td>' +
-                '<td>' + material.precioCompra + '</td>' +
-                '<td>' + material.precioVenta + '</td>' +
-                '<td>' + material.estatus + '</td>';
+                '<td>' + materialess.numeroUnicoMaterial + '</td>' +
+                '<td>' + materialess.nombreMaterial + '</td>' +
+                '<td>' + materialess.precioCompra + '</td>' +
+                '<td>' + materialess.precioVenta + '</td>' +
+                '<td>' + materialess.estatus + '</td>';
         cuerpotblMateriales += registroMateriales;
     });
     document.getElementById("tblMateriales").innerHTML = cuerpotblMateriales;
     cuerpotblMateriales = "";
 
+
+}
+export function cargarMaterialestblM() {
+    posMaterial = 0;
+    materiales.forEach(function (materialess) {
+        if (materialess.estatus !== 0) {
+            registroMateriales =
+            '<tr>' +
+            '<td>' + materialess.numeroUnicoMaterial + '</td>' +
+            '<td>' + materialess.nombreMaterial + '</td>' +
+            '<td>' + materialess.precioCompra + '</td>' +
+            '<td>' + materialess.precioVenta + '</td>' +
+            '<td>' + materialess.estatus + '</td>' +
+            '<td>' + '<a onclick="moduloMateriales.eliminarMaterial(' + posMaterial + ');"  class="btn btn-danger"><i class="fa fa-trash-o" aria-hidden="true"></i><a>' +
+            '<a onclick="moduloMateriales.modificarMaterial(' + posMaterial + ');"  class="btn btn-warning"><i class="fa fa-pencil" aria-hidden="true"></i><a></td>';
+            posMaterial++;
+            cuerpotblMateriales += registroMateriales;
+        }
+    });
+    document.getElementById("tblMateriales").innerHTML = cuerpotblMateriales;
+    cuerpotblMateriales = "";
 
 }
 
@@ -65,27 +84,102 @@ export function registrar(){
         });
 }
 
-export function eliminar(){
-    fetch("./Modulos/moduloMateriales/view_eliminarMaterial.html")
+export function eliminar() {
+    fetch("Modulos/moduloMateriales/view_eliminarMaterial.html")
         .then(function (respuesta) {
-        return respuesta.text();
+            return respuesta.text();
         })
         .then(function (html) {
-        document.getElementById("contenedorGestion").innerHTML = html;
-        cargarMaterialestbl();
+            document.getElementById("contenedorGestion").innerHTML = html;
+            import ("./controller_Material.js").then(
+                function (controller) {
+                    moduloMateriales = controller;
+                    moduloMateriales.cargarMaterialestblM();
+                    $('#table_id').DataTable();
+                    document.getElementById("table_id_filter").style.display = "block";
+
+                }
+        );
         });
 }
+export function modificar(pos) {
+    fetch("./Modulos/moduloMateriales/view_modificarMaterial.html")
+            .then(function (respuesta) {
+                return respuesta.text();
+            })
+            .then(function (html) {
+                document.getElementById("contenedorGestion").innerHTML = html;
+                let materialSelec = materiales[pos];
+                posMaterial = pos;
+                document.getElementById("nombreMaterial").value = materialSelec.nombreMaterial;
+                document.getElementById("precioCompra").value = materialSelec.precioCompra;
+                document.getElementById("precioVenta").value = materialSelec.precioVenta;
 
-export function catalogo(){
+            });
+
+
+}
+export function cargarMaterialestblC() {
+    let filtro = document.getElementById("filtro").value;
+    materiales.forEach(function (materialess) {
+        if (filtro === "A" && materialess.estatus !== 0) {
+            registroMateriales =
+            '<tr>' +
+            '<td>' + materialess.numeroUnicoMaterial + '</td>' +
+            '<td>' + materialess.nombreMaterial + '</td>' +
+            '<td>' + materialess.precioCompra + '</td>' +
+            '<td>' + materialess.precioVenta + '</td>' +
+            '<td>' + materialess.estatus + '</td>';
+
+            cuerpotblMateriales += registroMateriales;
+        } else if (filtro === "I" && materialess.estatus === 0) {
+            registroMateriales =
+            '<tr>' +
+            '<td>' + materialess.numeroUnicoMaterial + '</td>' +
+            '<td>' + materialess.nombreMaterial + '</td>' +
+            '<td>' + materialess.precioCompra + '</td>' +
+            '<td>' + materialess.precioVenta + '</td>' +
+            '<td>' + materialess.estatus + '</td>';
+
+            cuerpotblMateriales += registroMateriales;
+        } else if (filtro === "Am") {
+            registroMateriales =
+            '<tr>' +
+            '<td>' + materialess.numeroUnicoMaterial + '</td>' +
+            '<td>' + materialess.nombreMaterial + '</td>' +
+            '<td>' + materialess.precioCompra + '</td>' +
+            '<td>' + materialess.precioVenta + '</td>' +
+            '<td>' + materialess.estatus + '</td>';
+
+            cuerpotblMateriales += registroMateriales;
+
+        }
+        });
+    $('#table_id').DataTable();
+    document.getElementById("table_id_filter").style.display = "none";
+    document.getElementById("tblMateriales").innerHTML = cuerpotblMateriales;
+    cuerpotblMateriales = "";
+}
+export function catalogo() {
     fetch("./Modulos/moduloMateriales/view_catalogoMaterial.html")
-        .then(function (respuesta) {
-        return respuesta.text();
-        })
-        .then(function (html) {
-        document.getElementById("contenedorGestion").innerHTML = html;
-
-        });
-}
+            .then(function (respuesta) {
+            return respuesta.text();
+            })
+            .then(function (html) {
+            document.getElementById("contenedorGestion").innerHTML = html;
+            import ("./controller_Material.js").then(
+                    function (controller) {
+                    moduloMateriales = controller;
+                    $(document).ready(function(){
+                            moduloMateriales.cargarMaterialestbl();
+                            moduloMateriales.cargarMaterialestblC();
+                            $('#table_id').DataTable();
+                            document.getElementById("table_id_filter").style.display = "none";
+                    });
+                    }
+            );
+            });
+    }
 
 export function registrarNuevo() {
     let inputs = document.getElementsByTagName("input");
@@ -111,8 +205,8 @@ export function registrarNuevo() {
             denyButtonText: `No`
         }).then((result) => {
             if (result.isConfirmed) {
-                Swal.fire('!Material registrado con exito!', '', 'success')
-                alacenarDatos();
+                Swal.fire('¡Material registrado con exito!', '', 'success')
+                moduloMateriales.almacenarDatos();
             } else if (result.isDenied) {
 
             }
@@ -143,18 +237,75 @@ export function almacenarDatos() {
     let precioCompra = document.getElementById("precioCompra").value;
     let precioVenta = document.getElementById("precioVenta").value;
 
-    let persona = {
-        "IdPersona": idPersona,
-        "Nombre": nombres,
-        "ApellidoPaterno": apellidoP,
-        "ApellidoMaterno": apellidoM,
-        "Genero": genero,
-        "RFC": rfc,
-        "TelCasa": telCasa,
-        "TelMovil": telMovil,
-        "Correo": correo,
-        "Estatus": 1
+    let materia = {
+        "nombreMaterial": nombreMaterial,
+        "precioCompra": precioCompra,
+        "precioVenta": precioVenta,
+        "estatus": 1
     };
+
+    materiales.push(materia);
+
+    console.log(materiales);
+
+    material();
+}
+
+/*Modificar datos*/
+export function modificarMaterial() {
+    let inputs = document.getElementsByTagName("input");
+    let input;
+    let vacios = 0;
+    let comprobarString = true;
+    for (var i = 0; i < inputs.length; i++) {
+        input = inputs[i];
+
+        if (input.value === "") {
+            vacios++;
+        }
+
+    }
+
+    if (vacios === 0) {
+        if (comprobarString === true) {
+            Swal.fire({
+                title: '¿Seguro de modificar el material?',
+                showDenyButton: true,
+                confirmButtonText: 'Si',
+                denyButtonText: `No`
+            }).then((result) => {
+                if (result.isConfirmed) {
+                    Swal.fire('¡Material modificado con exito!', '', 'success')
+                    moduloMateriales.modificarDatos();
+                } else if (result.isDenied) {
+
+                }
+            });
+        } else {
+            Swal.fire({
+                title: 'Algo salió mal',
+                text: "¡Asegurese de que ningun campo que no requiera números no contenga ningun número!",
+                icon: 'warning',
+                confirmButtonColor: '#3085d6',
+                confirmButtonText: 'Aceptar!'
+            })
+        }
+
+    } else {
+        Swal.fire({
+            title: 'Algo salió mal',
+            text: "¡Asegurese de llenar todos los campos!",
+            icon: 'warning',
+            confirmButtonColor: '#3085d6',
+            confirmButtonText: 'Aceptar!'
+        })
+    }
+}
+export function modificarDatos() {
+    let nombreMaterial = document.getElementById("nombreMaterial").value;
+    let precioCompra = document.getElementById("precioCompra").value;
+    let precioVenta = document.getElementById("precioVenta").value;
+
     let material = {
         "nombreMaterial": nombreMaterial,
         "precioCompra": precioCompra,
@@ -162,7 +313,34 @@ export function almacenarDatos() {
         "estatus": 1
     };
 
-    materiales.push(material);
+    materiales[posMaterial]= materia;
+
+    console.log(materia);
 
     material();
+}
+
+/*Eliminar accesorio */
+export function eliminarMaterial(posMaterial) {
+    Swal.fire({
+        title: 'Advertencia',
+        text: "¿Esta seguro de eliminar a este accesorio?",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Si',
+        cancelButtonText: 'No'
+
+    }).then((result) => {
+        if (result.isConfirmed) {
+            materiales[posMaterial].estatus = 0;
+            moduloMateriales.eliminar();
+            Swal.fire(
+                    'Eliminado!',
+                    'El accesorio se ha eliminado con exito',
+                    'success'
+                    )
+        }
+    })
 }
